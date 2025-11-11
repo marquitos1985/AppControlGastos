6562ccbe7b2a117afeda7223804bf41d39c11fbc
marquitos1985 <marquitos1985@hotmail.com>
2025-11-11 20:20:18 -0300

    first commit

---
+++ src/main/java/com/AppDesarrollo/AppControlGastos/AppControlGastosApplication.java
@@ -0,0 +1,13 @@
+package com.AppDesarrollo.AppControlGastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppControlGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppControlGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AppDesarrollo/AppControlGastos/AppControlGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppDesarrollo.AppControlGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppControlGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
