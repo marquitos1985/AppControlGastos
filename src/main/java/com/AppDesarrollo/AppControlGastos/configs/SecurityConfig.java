609062e36d6b7f98be69fe5050c2893cf7daefa2
marquitos1985 <marquitos1985@hotmail.com>
2025-12-30 09:40:03 -0300

    SecurityConfig class

---
+++ src/main/java/com/AppDesarrollo/AppControlGastos/configs/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.AppDesarrollo.AppControlGastos.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+
+
+}
